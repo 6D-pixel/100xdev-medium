63fb461bfe9b6ab6f362a66c499ad6844d00daee
6D-pixel <dinesh7rd@gmail.com>
2024-07-18 12:19:50 +0530

    default page
    route added to page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup  from "./pages/Signup";
-import Signin  from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 import Blog from "./pages/Blog";
 import Blogs from "./pages/Blogs";
 import Publish from "./pages/Publish";
@@ -10,11 +10,12 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Signup />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
-          <Route path="/publish" element={<Publish/>}/>
+          <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
     </>
